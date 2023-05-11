03e047eb33c920c662792f34e2bedb9781d55ae8
Kevinmrozek <kmrozek0859@gmail.com>
2023-05-11 10:54:39 -0500

    complete cc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-// Your code here
+fetch('http://localhost:3000/characters')
+    .then((resp) => resp.json())
+    .then((characterObjects) => characterObjects.forEach((characters) => renderCharacter(characters)))
+
+const characters1 = document.querySelector('#character-bar')
+
+function renderCharacter(characters) {
+    const charactersCard = document.createElement('span')
+    charactersCard.className = 'card'
+    console.log(charactersCard)
+
+    const p = document.createElement('p')
+    p.textContent = characters.name
+    console.log(p)
+
+    const img = document.createElement('img')
+    img.src = characters.image
+    img.className = 'characters-avatar'
+    console.log(img)
+
+    const h4 = document.createElement('h4')
+    h4.textContent = `Total votes ${votes-form}`
+    console.log(h4)
+    
+    // charactersCard.append()
+    charactersCard.append(p)
+    charactersCard.append(img)
+    charactersCard.append(h4)
+    
+    characters1.appendChild(charactersCard)
+
+}
+
+const form = document.querySelector('#characterInfo')
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    const characterObject = {
+        "id": e.target.id.value,
+        "name": e.target.name.value,
+        "image": e.target.image.value,
+        "votes": e.target.votes.value
+    }
+
+    renderCharacter(characterObject)
+
+    e.target.reset()
+}
